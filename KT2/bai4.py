1285c0323fd2248376fdd55ea669728b9ecf5dfd
kimdamitung <150353472+kimdamitung@users.noreply.github.com>
2024-09-18 09:25:06 +0700

    add check port in jetson

--- KT2/bai4.py
+++ KT2/bai4.py
@@ -1,13 +1,63 @@
 from time import sleep
 from JETSON.RARM import RARM
+from JETSON.DCMotor import DCMotor
+import cv2
+from ultralytics import YOLO
 
+model = YOLO("best.pt")
+cap = cv2.VideoCapture(0)
 channels = ['9', '12', '17', '21', '24', '28']
 arm = RARM(port = 'COM13')
 
-while 1:
-    arm.setPos([2050, 2050, 1153, 2133, 1480, 1357])
-    sleep(2)
-    arm.setPos([1520, 2255, 745, 2010, 1480, 1600])
-    sleep(2)
-    arm.setPos([1031, 1806, 1398, 2133, 1480, 1888])
-    sleep(2)
+while True:
+    success, frame = cap.read()
+    if success:
+        results = model(frame)
+        names = []
+        
+        if len(results) == 0:
+            continue
+        result = results[0]
+        annotated_frame = result.plot()
+        for box in result.boxes:
+            class_id = int(box.cls)
+            object_name = model.names[class_id]
+            names.append(object_name)
+                
+        cv2.imshow("YOLO8 Interface", annotated_frame)
+        
+        try:
+            if "Hinh_tron_vang" in names and "So_2" in names and "Chu_d" in names:
+                arm.setPos([1500, 1500, 1500, 1500, 1500, 1500])
+                sleep(1e-1)
+            elif "Hinh_tron_vang" in names and "So_2" in names:    
+                arm.setPos([1520, 2255, 745, 2010, 1480, 1600])
+                sleep(1e-1)
+            elif "Hinh_tron_vang" in names and "Chu_d" in names:
+                arm.setPos([2050, 2050, 1153, 2133, 1480, 1357])
+                sleep(1e-1)
+            elif "So_2" in names and "Chu_d" in names:
+                arm.setPos([1031, 1806, 1398, 2133, 1480, 1888])
+                sleep(1e-1)
+            elif "Hinh_tron_vang" in names:            
+                arm.setPos([1520, 2255, 745, 2010, 1480, 1600])
+                sleep(1e-1)
+            elif "So_2" in names:
+                arm.setPos([2050, 2050, 1153, 2133, 1480, 1357])
+                sleep(1e-1)
+            elif "Chu_d" in names:
+                arm.setPos([1031, 1806, 1398, 2133, 1480, 1888])
+                sleep(1e-1)
+            else:
+                arm.setPos([1500, 1500, 1500, 1500, 1500, 1500])
+                sleep(1e-1)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except:
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
